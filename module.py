43adb4221489d3505d0b7aaf9e4a0d30303728d3
Febian22 <normalbian22@gmail.com>
2025-06-21 13:31:27 +0700

    add documentation

--- module.py
+++ module.py
@@ -2,12 +2,30 @@ from tabulate import tabulate
 
 class Membership:
     def show_benefit(self):
+        '''
+        Show all membership benefit in tabulated table (grid)
+
+        parameter:
+        None
+
+        Return:
+        None
+        '''
         self.membership_benefit = [{'Membership':'Platinum','Discount': '15%', 'Another Benefit': "Benefit Silver + Gold + Voucher Liburan + Cashback max. 30%"},
                               {'Membership':'Gold','Discount': '10%', 'Another Benefit': "Benefit Silver + Voucher Ojek Online"},
                               {'Membership':'Silver','Discount': '8%', 'Another Benefit': "Voucher Makanan"}]
         print(tabulate(self.membership_benefit, headers='keys', tablefmt='grid'))
     
     def show_requirement(self):
+        '''
+        show all membership requirement in tabulated table (grid)
+
+        Parameter:
+        None
+
+        Return:
+        None
+        '''
         self.membership_requirement = [{'Membership':'Platinum','Monthly Expense (Rp.)': 8000000, 'Monthly Income (Rp.)': 15000000},
                               {'Membership':'Gold','Monthly Expense (Rp.)': 6000000, 'Monthly Income (Rp.)': 10000000},
                               {'Membership':'Silver','Monthly Expense (Rp.)': 5000000, 'Monthly Income (Rp.)': 7000000}]
@@ -15,6 +33,17 @@ class Membership:
         print(tabulate(self.membership_requirement,headers='keys', tablefmt='grid', intfmt=","))
     
     def predict_membership(self,expense:int,income:int):
+        '''
+        Predict possible membership for user based on the monthly expense and income
+        using Euclidean distance
+
+        parameter:
+        - Expense : Int
+        - Income : Int
+
+        return:
+        None
+        '''
         if type(expense) == int and type(expense) == int:
             pass
         else:
@@ -30,6 +59,15 @@ class Membership:
         
     
     def calculate_price(self, membership:str, price_list):
+        '''
+        Calculate price after discount for the member
+
+        parameter:
+        - membership : str (typo result in null disc)
+
+        return:
+        None
+        '''
         disc = 0
         for item in self.membership_benefit:
             if item['Membership'] == membership.capitalize():
@@ -38,12 +76,4 @@ class Membership:
         
         print(f"Total belanja anda : Rp. {int(sum(price_list)*(1-disc))}")
 
-
-if __name__ == '__main__':
-    user1 = Membership()
-    user1.show_benefit()
-    user1.show_requirement()
-    user1.predict_membership(3_000_000,14_000_000)
-    belanja = [1_000_000, 2_000_000, 2_000_000]
-    user1.calculate_price('Platinum',belanja)
 
